0c705e92907a61704786dfad0b474d38c205b8f6
Hettomei <itsumo.sibyllin@gmail.com>
2014-02-25 15:21:17 +0100

    Revert  commit 3d7f81b non fonctionnel avec nos form

--- app/Global.java
+++ app/Global.java
@@ -5,9 +5,5 @@ import play.api.mvc.EssentialFilter;
 import play.filters.csrf.CSRFFilter;
 
 public class Global extends GlobalSettings {
-  @Override
-  public <T extends EssentialFilter> Class<T>[] filters() {
-    return new Class[]{CSRFFilter.class};
-  }
 
 }

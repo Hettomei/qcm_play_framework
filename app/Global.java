ea457f859d275381205968ebfbf9f4c6aaf56458
unknown <d134ganner@25-UC27-15.stagiaires.local>
2014-02-25 16:32:08 +0100

    creation partie Admin

--- app/Global.java
+++ app/Global.java
@@ -2,7 +2,6 @@ import play.Application;
 import play.Logger;
 import play.GlobalSettings;
 import play.api.mvc.EssentialFilter;
-import play.filters.csrf.CSRFFilter;
 
 public class Global extends GlobalSettings {
 
--- app/controllers/QcmController.java
+++ app/controllers/QcmController.java
@@ -14,27 +14,6 @@ public class QcmController extends Controller {
 	static Form<Qcm> theForm = Form.form(Qcm.class);
 
 	public static Result index() {
-		return redirect(routes.QcmController.qcms());
+		return ok("ok");
 	}
-	public static Result qcms() {
-		return ok(views.html.index.render(Qcm.all(), theForm));
-	}
-
-	public static Result newQcm() {
-		Form<Qcm> filledForm = theForm.bindFromRequest();
-		if(filledForm.hasErrors()) {
-			return badRequest(
-					views.html.index.render(Qcm.all(), filledForm)
-					);
-		} else {
-			Qcm.create(filledForm.get());
-			return redirect(routes.QcmController.qcms());  
-		}
-	}
-
-	public static Result deleteQcm(Long id) {
-		Qcm.delete(id);
-		return redirect(routes.QcmController.qcms());
-	}
-
 }

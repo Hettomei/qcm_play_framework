1a418f42cd4922879f3c7cf58c490906a394be61
unknown <d134ganner@25-UC27-15.stagiaires.local>
2014-02-26 11:42:37 +0100

    Mise en place du menu actif

---
+++ app/controllers/admin/ProvisoireController.java
@@ -0,0 +1,10 @@
+package controllers.admin;
+
+import play.mvc.*;
+
+public class ProvisoireController extends AdminController {
+
+	public static Result index() {
+		return ok("ok ProvisoireController ");
+	}
+}

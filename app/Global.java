d6dc30395edc874630448aa78e59fedf6a4b98c0
unknown <d134ganner@25-UC27-15.stagiaires.local>
2014-02-27 15:29:51 +0100

    Yaml stagiaire

--- app/Global.java
+++ app/Global.java
@@ -13,6 +13,23 @@ import com.avaje.ebean.*;
 public class Global extends GlobalSettings {
 
 	public void onStart(Application app) {
+		InitialData.insert(app);
 	}
 
+	static class InitialData {
+
+		public static void insert(Application app) {
+			if(Ebean.find(Stagiaire.class).findRowCount() == 0) {
+
+				@SuppressWarnings("unchecked")
+				Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+				// Insert users first
+				Ebean.save(all.get("stagiaires"));
+
+
+
+			}
+		}
+
+	}
 }

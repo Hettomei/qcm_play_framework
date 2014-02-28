0db4f06be2a7a43b72c14bf2429962b4bc72ef1c
unknown <d134ganner@25-UC27-15.stagiaires.local>
2014-02-28 11:36:16 +0100

    Ajout du qcm par stagiaire

--- app/controllers/admin/StagiairesController.java
+++ app/controllers/admin/StagiairesController.java
@@ -19,6 +19,29 @@ public class StagiairesController extends AdminController {
 
 	public static Result show(Long id) {
 		Stagiaire s = models.Stagiaire.find.ref(id);
-		return ok(views.html.admin.Stagiaire.show.render(s, theForm));
+		return ok(views.html.admin.Stagiaire.show.render(s, models.Qcm.all()));
+	}
+	
+	public static Result add_qcm(Long id) {
+		DynamicForm requestData = Form.form().bindFromRequest();
+		long qcm_id = Long.parseLong(requestData.get("qcm_id"),10);
+		models.Stagiaire sta = models.Stagiaire.find.byId(id);
+		models.Qcm qcm = models.Qcm.find.byId(qcm_id);
+		sta.qcms.add(qcm);
+		sta.save();
+
+		return redirect(controllers.admin.routes.StagiairesController.show(id));
+	}
+
+	public static Result remove_qcm(Long id) {
+		DynamicForm requestData = Form.form().bindFromRequest();
+		long qcm_id = Long.parseLong(requestData.get("qcm_id"),10);
+		models.Stagiaire sta = models.Stagiaire.find.byId(id);
+		models.Qcm qcm = models.Qcm.find.byId(qcm_id);
+		
+		sta.qcms.remove(qcm);
+		sta.save();
+
+		return redirect(controllers.admin.routes.StagiairesController.show(id));
 	}
 }

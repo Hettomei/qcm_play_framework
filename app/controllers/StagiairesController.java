7653aa91e2e73b6620d2d1e490ef5a1ace31372e
unknown <d134ganner@25-UC27-15.stagiaires.local>
2014-03-03 11:03:30 +0100

    Ajout du login Stagiaire en session

--- app/controllers/StagiairesController.java
+++ app/controllers/StagiairesController.java
@@ -1,17 +1,45 @@
 package controllers;
 
+import javax.servlet.http.HttpSession;
+import models.Stagiaire;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.*;
+import play.mvc.Http.Context;
+import play.mvc.Http.Response;
+import play.mvc.Http.Session;
 
 public class StagiairesController extends Controller {
 
 	static Form<models.Stagiaire> theForm = Form.form(models.Stagiaire.class);
-	
-	public static Result index() {
-		   return ok(views.html.stagiaire.index.render(theForm));
+
+	public static Result show() {
+		Session session = Http.Context.current().session();
+		String id = session.get("login");
+
+		if (id!=(null)) {
+			Stagiaire s = models.Stagiaire.find.byId(Long.parseLong(id));
+			if (s.equals(null)){
+				return ok(views.html.stagiaire.sessiontest.render());
+			}else{
+				return ok(views.html.stagiaire.show.render(s));
+			}
+		}else{
+			return ok(views.html.stagiaire.sessiontest.render());
+		}
 	}
-	
+
+	public static Result sessiontest() {
+		Session session = Http.Context.current().session();
+		DynamicForm requestData = Form.form().bindFromRequest();
+		String id = requestData.get("login"); 
+		session.put("login", id );
+		return redirect(controllers.routes.StagiairesController.show());
+	}
+
 	public static Result disconnect() {
-		   return ok("Déconnexion ! ahah");
+		Session session = Http.Context.current().session();
+	    session.remove("login");
+	    return redirect(controllers.routes.StagiairesController.show());
 	}
 }
